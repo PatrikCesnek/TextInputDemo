bc820c8220dae33f7ef35d925479ec4dbe67b7c9
Patrik Cesnek <pcesnek290@gmail.com>
2025-04-08 12:08:04 +0200

    Add color assets, add dimensions

--- TextInputDemo/Constants/Constants.swift
+++ TextInputDemo/Constants/Constants.swift
@@ -5,16 +5,46 @@
 //  Created by Patrik Cesnek on 08/04/2025.
 //
 
-import Foundation
+import SwiftUI
 
 struct Constants {
     
-}
-
-struct Fonts {
+    struct Fonts {
+        static let labelM = Font.custom("Inter-Medium", size: 16)
+        static let labelS = Font.custom("Inter-SemiBold", size: 14)
+        static let bodyM = Font.custom("Inter-Regular", size: 16)
+    }
     
-}
-
-struct Colors {
+    struct Colors {
+        // Surface
+        static let surfaceXHigh = Color("surfaceXHigh")
+        static let surfaceXLow = Color("surfaceXLow")
+        static let surfaceBand = Color("surfaceBand")
+        static let surfaceDanger = Color("surfaceDanger")
+        static let surfaceDangerVariant = Color("surfaceDangerVariant")
+        static let surfaceWarning = Color("surfaceWarning")
+        static let surfaceWarningVariant = Color("surfaceWarningVariant")
+        
+        // Content
+        static let contentOnNeutralXXHigh = Color("contentOnNeutralXXHigh")
+        static let contentOnNeutralMedium = Color("contentOnNeutralMedium")
+        static let contentOnNeutralLow = Color("contentOnNeutralLow")
+        static let contentOnNeutralDanger = Color("contentOnNeutralDanger")
+        static let contentOnNeutralWarning = Color("contentOnNeutralWarning")
+        
+        // State
+        static let stateDefaultHover = Color("stateDefaultHover")
+        static let stateDefaultFocus = Color("stateDefaultFocus")
+    }
     
+    struct Dimensions {
+        // Radius
+        static let input: CGFloat = 12
+        
+        // Spacing
+        static let dimensionXs: CGFloat = 8
+        static let dimensionS: CGFloat = 12
+        static let dimensionM: CGFloat = 16
+        static let dimensionL: CGFloat = 20
+    }
 }
