3faf334af7b73a9eb98ab61910e88ffe6e22301a
Patrik Cesnek <pcesnek290@gmail.com>
2025-04-08 11:04:34 +0200

    Initial Commit

---
+++ TextInputDemo/TextInputDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextInputDemoApp.swift
+//  TextInputDemo
+//
+//  Created by Patrik Cesnek on 08/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct TextInputDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
