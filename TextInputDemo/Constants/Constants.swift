ff61324c41adc8430b7c862cda494f753f363383
Patrik Cesnek <pcesnek290@gmail.com>
2025-04-08 11:09:58 +0200

    Create constants file

---
+++ TextInputDemo/Constants/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  TextInputDemo
+//
+//  Created by Patrik Cesnek on 08/04/2025.
+//
+
+import Foundation
+
+struct Constants {
+    
+}
+
+struct Fonts {
+    
+}
+
+struct Colors {
+    
+}
